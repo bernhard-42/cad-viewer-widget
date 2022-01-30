4c3275674b9e6594be5e43cd95bc7339f4944202
Bernhard <b_walter@arcor.de>
2022-01-30 16:01:54 +0100

    fix linter issue

--- js/lib/index.js
+++ js/lib/index.js
@@ -1,6 +1,8 @@
 // Export widget models and views, and the npm package version number.
 
+// eslint-disable-next-line no-undef
 var widgetExports = require("./widget.js");
+// eslint-disable-next-line no-undef
 var sidecarExports = require("./sidecar.js");
 
 // eslint-disable-next-line no-undef
