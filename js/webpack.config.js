6301a7f9cd6628ba80c82117104f5234cf16c410
Bernhard <b_walter@arcor.de>
2023-08-14 08:29:16 +0200

    switch to pyproject.toml and hatch. Remove notebook <7 support

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -16,68 +16,6 @@ module.exports = (env, argv) => {
   var devtool = argv.mode === "development" ? "source-map" : false;
   return [
     {
-      // Notebook extension
-      //
-      // This bundle only contains the part of the JavaScript that is run on
-      // load of the notebook. This section generally only performs
-      // some configuration for requirejs, and provides the legacy
-      // "load_ipython_extension" function which is required for any notebook
-      // extension.
-      //
-      entry: "./lib/extension.js",
-      output: {
-        filename: "extension.js",
-        path: path.resolve(__dirname, "..", "cad_viewer_widget", "nbextension"),
-        libraryTarget: "amd",
-        publicPath: "" // publicPath is set in extension.js
-      },
-      optimization: {
-        minimize: minimize,
-        minimizer: [
-          new TerserPlugin({
-            parallel: true,
-            terserOptions: {
-              compress: { defaults: false },
-              mangle: true
-            }
-          })
-        ]
-      },
-      devtool
-    },
-    {
-      // Bundle for the notebook containing the custom widget views and models
-      //
-      // This bundle contains the implementation for the custom widget views and
-      // custom widget.
-      // It must be an amd module
-      //
-      entry: "./lib/index.js",
-      output: {
-        filename: "index.js",
-        path: path.resolve(__dirname, "..", "cad_viewer_widget", "nbextension"),
-        libraryTarget: "amd",
-        publicPath: ""
-      },
-      devtool,
-      optimization: {
-        minimize: minimize,
-        minimizer: [
-          new TerserPlugin({
-            parallel: true,
-            terserOptions: {
-              compress: { defaults: false },
-              mangle: true
-            }
-          })
-        ]
-      },
-      module: {
-        rules: rules
-      },
-      externals: ["@jupyter-widgets/base"]
-    },
-    {
       // Embeddable cad-viewer-widget bundle
       //
       // This bundle is generally almost identical to the notebook bundle
